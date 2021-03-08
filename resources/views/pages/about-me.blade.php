2a60a932c5f35d9937a5d2ed1543a8f90cc1ebdb
Simon Frezard <simon.frezard@gmail.com>
2021-03-08 15:52:44 +0100

    use asset function to make use of CDN, in real app will make the loading way faster

--- resources/views/pages/about-me.blade.php
+++ resources/views/pages/about-me.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
   <img class="w-32 h-32 rounded-full"
-  src="/images/profil_simon.jpg" alt="image profil">
+src="{{ asset('/images/profil_simon.jpg') }}" alt="image profil">
   <p>Built with Laravel8 &hearts; by Simon F. checkout my <a href="https://github.com/Mimicmi" target="_blank">github</a> !</p>
   <p><a href="{{ route('home') }}">Return to homepage</a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,7 @@
 @section('content')
   <div class="border border-light-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
     <img class="object-none h-48 w-full"
-    src="/images/lyon.jpg" alt="photo lyon">
+  src="{{ asset('/images/lyon.jpg') }}" alt="photo lyon">
   </div>
   <h1>Hello from Lyon !</h1>
   <p>It's {{ date('h:i A') }} in Lyon !</p>
