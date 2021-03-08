448a7da1c08dadb4ae9e35beb3f5bf6b6277c478
Simon Frezard <simon.frezard@gmail.com>
2021-03-08 11:02:15 +0100

    modified basic welcome view to Home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Hi My City !</title>
+  </head>
+
+  <body>
+    <h1>Hello from Lyon</h1>
+    <p>It's 'insert time from function' in Lyon !</p>
+  </body>
+
+  <footer>
+    <p>&copy; Copyright 2021 &middot; <a href="#">About us</a> </p>
+  </footer>
+</html>
