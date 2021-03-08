fc4d82463d7b514f78c68300c29946496781e190
Simon Frezard <simon.frezard@gmail.com>
2021-03-08 15:08:49 +0100

    added images to view

--- resources/views/pages/about-me.blade.php
+++ resources/views/pages/about-me.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'About me | ' . config('app.name'))
 
 @section('content')
+  <img src="/images/profil_simon.jpg" alt="image profil" height="50%" width="50%">
   <p>Built with Laravel8 &hearts; by Simon F. checkout my <a href="https://github.com/Mimicmi" target="_blank">github</a> !</p>
-<p><a href="{{ route('home') }}">Return to homepage</a></p>
+  <p><a href="{{ route('home') }}">Return to homepage</a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,7 @@
 @section('title')
 
 @section('content')
+    <img src="/images/lyon.jpg" alt="photo lyon" height="50%" width="50%">
     <h1>Hello from Lyon !</h1>
     <p>It's {{ date('h:i A') }} in Lyon !</p>
 @endsection
